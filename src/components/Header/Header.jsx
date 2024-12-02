b83a04c4927773a2f0301207f76158a3c7d657ef
m1903164 <johnfrontend001@gmail.com>
2024-12-02 21:23:11 +0300

    add styles in components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+
 import Button from "../Button/Button"
-import {useTelegram} from "../../hooks/useTelegram";
+import {useTelegram} from "../../hooks/useTelegram"
+
+import './Header.css'
 
 const Header = () => {
     const { user, onClose } = useTelegram()
